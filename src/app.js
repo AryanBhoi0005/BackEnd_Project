3b9bb95a44070ef748d825a45bf2b137110355f7
AryanBhoi0005 <aryanbhoi1962@gmail.com>
2024-07-26 02:16:32 +0530

    Controllers and Routing

--- src/app.js
+++ src/app.js
@@ -13,4 +13,23 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.static("public"))
 app.use(cookieParser())
 
+
+
+
+//routes
+import userRouter from './routes/user.routes.js'
+
+//Routes declaration 
+app.use("/api/v1/users",userRouter) 
+
+//Giving control to userRouter
+// http://localhost:8000/users then route to user.routes 
+// then we will http://localhost:8000/users/registers so it will call the route mentioned  
+
+//We are simply passing the control to the user where we can add the route as per our requirements /login , /register
+
+
+
+
+
 export {app}
