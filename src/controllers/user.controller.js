1bebf40b0517ac9404792efb2a81c6c72ce0a0a5
AryanBhoi0005 <aryanbhoi1962@gmail.com>
2024-08-03 17:06:35 +0530

    Aggregation pipeline added along with SubsCount and watchHistory

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -5,6 +5,7 @@ import {uploadOnCloudinary} from "../utils/cloudinary.js";
 import { ApiResponse } from "../utils/ApiResponses.js";
 import JsonWebTokenError  from "jsonwebtoken";
 import { response } from "express";
+import mongoose from "mongoose";
 
 //We are going to use it over and over again so we are making it as methods
 const generateAccessandRefreshTokens= async(userId)=>{ //Func takes in userID
@@ -270,7 +271,13 @@ const changeCurrentPassword = asyncHandler(async(req,res)=>{
 const getCurrentUser=asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(
+        new ApiResponse(
+            200,
+            req.user,
+            "Current user fetched successfully"
+        )
+    )
 })
 
 const updateAccountDetails=asyncHandler(async(req,res)=>{
@@ -279,7 +286,7 @@ const updateAccountDetails=asyncHandler(async(req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
     //Callin mongoDB operators
-    const user=User.findByIdAndUpdate(
+    const user= await User.findByIdAndUpdate(
         req.user?._id,
         {
              $set:{
@@ -353,6 +360,150 @@ const updateUserCoverIamge=asyncHandler(async(req,res)=>{
     )
 })
 
+const getUserChannelProfile=asyncHandler(async(req,res)=>{
+    //When we need profile we usually visit channel url
+    //params is used to get info from its url
+       const {username}=req.params
+       if(!username?.trim()){
+        throw new ApiError(400,"username is missing")
+       }
+    //Aggregate pipeline return arrays
+       const channel=await User.aggregate([
+        //Stage 1
+         {
+            $match:{
+                username:username?.toLowerCase()
+            }
+         },
+        //Stage 2
+         {   
+            //subs model is the doc created everytime we sub channel
+            $lookup:{
+                //We convert Subscription to below field as in mongo it turns to lowercase and pural
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"channel",
+                as:"subscribers"
+            }
+         },
+         //Stage 3
+         {
+            $lookup:{
+                from:"subscriptions",
+                localField:"_id",
+                foreignField:"subscriber",
+                as:"subscribedTo"
+            }
+         },
+         //Stage 4
+         {
+            $addFields:{
+                subscribersCount:{
+                    //returns count of subs with size
+                    $size:"$subscribers"
+                },
+                channelsSubscribedToCount:{
+                    $size:"$subscribedTo"
+                },
+                //To check if we are subs or not to a channel
+                isSubscribed:{
+                    $cond:{
+                        if:{$in:[req.user?._id,"$subscribers.subscriber"]},
+                //if subs place it in false else add in subs
+                        then:true,
+                        else:false
+                    }
+                }
+            }
+         },
+         {
+        //Projection of user profile as per our req
+        //1 here states we want to include that field
+        //Similarly 0 to exclude that field
+            $project:{
+            fullName:1,
+            username:1,
+            subscribersCount:1,
+            channelsSubscribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1
+         }
+
+         }
+
+       ])
+
+       if(!channel?.length){
+          throw new ApiError(404,"channel does not exist")
+       }
+       return res.status(200)
+       .json(
+        new ApiResponse(200,channel[0],"User channel fetched successfully")
+       )
+})
+
+const getWatchHistory=asyncHandler(async(req,res)=>{
+    const user=await User.aggregate([
+    //Stage 1
+     {
+        $match:{
+            _id:new mongoose.Types.ObjectId(req.user._id)
+        }
+     },
+    //Stage 2
+     {
+        $lookup:{
+            from:"videos",
+            localField:"watchHistory",
+            foreignField:"_id",
+            as:"watchHistory",
+            //Adding sub pipeline
+            pipeline:[
+                {
+                    $lookup:{
+                        from:"users",
+                        localField:"owner",
+                        foreignField:"-id",
+                        as:"owner",
+                        pipeline:[
+                            {
+                                $project:{
+                                    fullName:1,
+                                    username:1,
+                                    avatar:1
+                                }
+                            }
+                        ]
+                    }
+                },
+                {//The data is stored in owner field
+                  $addFields:{
+                    owner:{
+                        $first:"$owner"
+                    }
+                  }
+                }
+            ]
+        }
+     },
+    //Stage 3
+     {
+
+     }
+    ])
+
+    return res.status(200)
+    .json(
+        new ApiResponse(
+            200,
+            user[0].getWatchHistory,
+            "Watch History fetched successfully"
+        )
+    )
+})
+
 
 
 export {
@@ -364,6 +515,8 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverIamge
+    updateUserCoverIamge,
+    getUserChannelProfile,
+    getWatchHistory
 
 }
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from "../controllers/user.controller.js";
+import { loginUser, logoutUser, registerUser, refreshAccessToken, changeCurrentPassword, getCurrentUser, updateAccountDetails, updateUserAvatar, updateUserCoverIamge, getUserChannelProfile, getWatchHistory } from "../controllers/user.controller.js";
 import {upload} from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 
@@ -26,6 +26,21 @@ router.route("/logout").post(verifyJWT,logoutUser)
 //After verifyJWT next() is used to tell the control to run logoutUser function
 
 router.route("/refresh-token").post(refreshAccessToken)
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT,updateAccountDetails)
+
+router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
+
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateUserCoverIamge)
+
+//While using params we have to specify the name already specified no changes in it in a specific manner
+router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
+
+router.route("/history").get(verifyJWT,getWatchHistory)
 
 
 export default router
